c4d54dc6efc3e86b48ec8780fa3c1cd713eed2a0
Amol Bhave <ammubhave@gmail.com>
2016-08-19 17:56:25 -0400

    add oidc project stuff

--- project/settings.py
+++ project/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
-    'welcome',
+    'project',
+    'oidc_provider',
 )
 
 MIDDLEWARE_CLASSES = (
--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,15 @@
-from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from welcome.views import index, health
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^accounts/login/$', auth_views.login, { 'template_name': 'login.html' }, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, { 'next_page': '/' }, name='logout'),
 
-    url(r'^$', index),
-    url(r'^health$', health),
+    url(r'^', include('oidc_provider.urls', namespace='oidc_provider')),
+
     url(r'^admin/', include(admin.site.urls)),
 ]

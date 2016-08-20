580bb3f5fad225c8c2b197138044aa51695bdaed
Amol Bhave <ammubhave@gmail.com>
2016-08-19 22:50:22 -0400

    add sls ver

--- project/urls.py
+++ project/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^accounts/login/$', auth_views.login, { 'template_name': 'login.html' }, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, { 'next_page': '/' }, name='logout'),
 
+    url(r'.well-known/acme-challenge/w0b1ixLKVtuyJKE9FSh39UpaWhpHBk9yG6Segp0s2lA',
+        TemplateView.as_view(template_name='w0b1ixLKVtuyJKE9FSh39UpaWhpHBk9yG6Segp0s2lA')),
     url(r'^', include('oidc_provider.urls', namespace='oidc_provider')),
 
     url(r'^admin/', include(admin.site.urls)),

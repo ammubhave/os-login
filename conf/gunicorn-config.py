b7d2e3f29a8c0d9f8ea042bac9282a727ad9224b
Amol Bhave <ammubhave@gmail.com>
2016-08-20 00:44:15 -0400

    try enable ssl

--- conf/gunicorn-config.py
+++ conf/gunicorn-config.py
@@ -1,12 +1,12 @@
 import os
 import ssl
 
-# keyfile = '/tmp/ssl_private_key'
-# with open(keyfile, 'w') as keyf:
-#     keyf.write(os.environ.get('SSL_PRIVATE_KEY', ''))
-#
-# certfile = '/tmp/ssl_certificate'
-# with open(certfile, 'w') as certf:
-#     certf.write(os.environ.get('SSL_CERTIFICATE', ''))
-#
-# cert_reqs = ssl.CERT_OPTIONAL
+keyfile = '/tmp/ssl_private_key'
+with open(keyfile, 'w') as keyf:
+    keyf.write(os.environ.get('SSL_PRIVATE_KEY', ''))
+
+certfile = '/tmp/ssl_certificate'
+with open(certfile, 'w') as certf:
+    certf.write(os.environ.get('SSL_CERTIFICATE', ''))
+
+cert_reqs = ssl.CERT_OPTIONAL

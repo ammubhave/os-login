abd8c610f88f6d3a222d47adbc21d34f6f26151c
Amol Bhave <ammubhave@gmail.com>
2016-08-20 00:52:53 -0400

    add debug for ssl

--- conf/gunicorn-config.py
+++ conf/gunicorn-config.py
@@ -3,10 +3,12 @@ import ssl
 
 keyfile = '/tmp/ssl_private_key'
 with open(keyfile, 'w') as keyf:
+    print(os.environ.get('SSL_PRIVATE_KEY'))
     keyf.write(os.environ.get('SSL_PRIVATE_KEY', ''))
 
 certfile = '/tmp/ssl_certificate'
 with open(certfile, 'w') as certf:
+    print(os.environ.get('SSL_CERTIFICATE'))
     certf.write(os.environ.get('SSL_CERTIFICATE', ''))
 
 cert_reqs = ssl.CERT_OPTIONAL
